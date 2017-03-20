5929caa9aad81b3994c442fee3341a916679151f
Shayde Nofziger <sln7755@rit.edu>
2017-03-20 12:15:48 -0400

    Initial User, Course, and Course_User tables.

--- Database/GeneralTables.sql
+++ Database/GeneralTables.sql
@@ -1,18 +1,32 @@
 /* General tables used by all groups */
-
 /* ID field must be and INTEGER and not an INT to increment correctly */
-CREATE TABLE STUDENT(
+
+/* A User is anyone with a login to the system */
+/* Students should have IS_PROF = 0, IS_ADMIN = 0 */
+CREATE TABLE User(
 	ID INTEGER PRIMARY KEY,
 	USERNAME TEXT NOT NULL,
 	PASSWORD TEXT NOT NULL,
-	FIRSTNAME TEXT,
-	LASTNAME TEXT,
-	EMAIL TEXT
+	FIRSTNAME TEXT NOT NULL,
+	LASTNAME TEXT NOT NULL,
+	EMAIL TEXT NOT NULL,
+	IS_PROF BIT DEFAULT 0, /* If not provided, assume they are not a professor */
+	IS_ADMIN BIT DEFAULT 0 /* If not provided, assume they are not an admin */
 );
 
-CREATE TABLE ADMIN(
+/* Course Table. Add fields as needed. */
+/* Professor references the User ID of the Prof teaching the course. */
+CREATE TABLE Course(
 	ID INTEGER PRIMARY KEY,
-	USERNAME TEXT NOT NULL,
-	PASSWORD TEXT NOT NULL
+	NAME TEXT NOT NULL,
+	PROFESSOR_ID INTEGER NOT NULL,
+	FOREIGN KEY(PROFESSOR_ID) REFERENCES User(ID)
 );
 
+/* Join table. Student to Course relation */
+CREATE TABLE Course_User(
+	COURSE_ID INTEGER NOT NULL,
+	USER_ID INTEGER NOT NULL,
+	FOREIGN KEY(COURSE_ID) REFERENCES Course(ID),
+	FOREIGN KEY(USER_ID) REFERENCES User(ID)
+);
