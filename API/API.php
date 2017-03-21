045d8bfb6114ad707adc3ddfbf322c5d43ad8af9
Zachary Moran <zjm1065@rit.edu>
2017-03-21 15:46:13 -0400

    Stubbed out initial Enrollment Functions

--- API/API.php
+++ API/API.php
@@ -178,18 +178,282 @@ function facility_management_switch()
 function student_enrollment_switch()
 {
 	// Define the possible Student Enrollment function URLs which the page can be accessed from
-	$possible_function_url = array();
+	$possible_function_url = array("getStudent", "postStudent", "getInstructor", "getAdmin", "getCourse",
+									"postCourse", "getCourseList", "toggleCourse", "getSection", "getCourseSections",
+									"postSection", "deleteSection", "getSectionList", "getStudentGrades", "getStudentSections",
+									"getInstructorSections", "getBook", "getSectionBook", "getRoom", "getCurrentTerm", "getTerm",
+									"postTerm", "enrollStudent", "waitlistStudent", "postBook", "withdrawStudent");
 
 	if (isset($_GET["function"]) && in_array($_GET["function"], $possible_function_url))
 	{
 		switch ($_GET["function"])
 		{
+			case "getStudent":
+				// if has params
+				return getStudent();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "postStudent":
+				// if has params
+				return postStudent();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getInstructor":
+				// if has params
+				return getInstructor();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getAdmin":
+				// if has params
+				return getAdmin();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getCourse":
+				// if has params
+				return getCourse();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "postCourse":
+				// if has params
+				return postCourse();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getCourseList":
+				// if has params
+				return getCourseList();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "toggleCourse":
+				// if has params
+				return toggleCourse();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getSection":
+				// if has params
+				return getSection();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getCourseSections":
+				// if has params
+				return getCourseSections();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "postSection":
+				// if has params
+				return postSection();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "deleteSection":
+				// if has params
+				return deleteSection();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getSectionList":
+				// if has params
+				return getSectionList();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getStudentGrades":
+				// if has params
+				return getStudentGrades();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getStudentSections":
+				// if has params
+				return getStudentSections();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getInstructorSections":
+				// if has params
+				return getInstructorSections();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getBook":
+				// if has params
+				return getBook();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getSectionBook":
+				// if has params
+				return getSectionBook();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getRoom":
+				// if has params
+				return getRoom();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getCurrentTerm":
+				// if has params
+				return getCurrentTerm();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "getTerm":
+				// if has params
+				return getTerm();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "postTerm":
+				// if has params
+				return postTerm();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "enrollStudent":
+				// if has params
+				return enrollStudent();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "waitlistStudent":
+				// if has params
+				return waitlistStudent();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "postBook":
+				// if has params
+				return postBook();
+				// else
+				// return "Missing " . $_GET["param-name"]
+			case "withdrawStudent":
+				// if has params
+				return withdrawStudent();
+				// else
+				// return "Missing " . $_GET["param-name"]
 			
 		}
 	}
 }
 
-//Define Functions Here
+//Student Enrollment Functions
+
+function getStudent()
+{
+	return "TODO";
+}
+
+function postStudent()
+{
+	return "TODO";
+}
+
+function getInstructor()
+{
+	return "TODO";
+}
+
+function getAdmin()
+{
+	return "TODO";
+}
+
+function getCourse()
+{
+	return "TODO";
+}
+
+function postCourse()
+{
+	return "TODO";
+}
+
+function getCourseList()
+{
+	return "TODO";
+}
+
+function toggleCourse()
+{
+	return "TODO";
+}
+
+function getSection()
+{
+	return "TODO";
+}
+
+function getCourseSections()
+{
+	return "TODO";
+}
+
+function postSection()
+{
+	return "TODO";
+}
+
+function deleteSection()
+{
+	return "TODO";
+}
+
+function getSectionList()
+{
+	return "TODO";
+}
+
+function getStudentGrades()
+{
+	return "TODO";
+}
+
+function getStudentSections()
+{
+	return "TODO";
+}
+
+function getInstructorSections()
+{
+	return "TODO";
+}
+
+function getBook()
+{
+	return "TODO";
+}
+
+function getSectionBook()
+{
+	return "TODO";
+}
+
+function getRoom()
+{
+	return "TODO";
+}
+
+function getCurrentTerm()
+{
+	return "TODO";
+}
+
+function getTerm()
+{
+	return "TODO";
+}
+
+function postTerm()
+{
+	return "TODO";
+}
+
+function enrollStudent()
+{
+	return "TODO";
+}
+
+function waitlistStudent()
+{
+	return "TODO";
+}
+
+function postBook()
+{
+	return "TODO";
+}
+
+function withdrawStudent()
+{
+	return "TODO";
+}
 
 
 
