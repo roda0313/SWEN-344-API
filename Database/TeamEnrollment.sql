53b0a782313d1bccdf5082700bb580f3aab63ab4
Zachary Moran <zjm1065@rit.edu>
2017-03-24 12:11:11 -0400

    Added Enrollment DB Tables

--- Database/TeamEnrollment.sql
+++ Database/TeamEnrollment.sql
@@ -1,1 +1,36 @@
 /* Team Enrollment tables */
+/* ID field must be and INTEGER and not an INT to increment correctly */
+
+/* Join table. Student to Section relation */
+CREATE TABLE Waitlist(
+	SECTION_ID INTEGER NOT NULL,
+	STUDENT_ID INTEGER NOT NULL,
+	ADDED_DATE DATE,
+	FOREIGN KEY(SECTION_ID) REFERENCES Section(ID),
+	FOREIGN KEY(STUDENT_ID) REFERENCES Student(ID)
+);
+
+/* Schedule Table. */
+CREATE TABLE Schedule(
+	SECTION_ID INTEGER NOT NULL,
+	DAY_OF_WEEK CHAR VARYING,
+	START_TIME TIME,
+	END_TIME TIME,
+	FOREIGN KEY(SECTION_ID) REFERENCES Section(ID)
+);
+
+/* Term table. */
+CREATE TABLE Term(
+	ID INTEGER PRIMARY KEY,
+	CODE CHAR VARYING,
+	START_DATE DATE,
+	END_DATE DATE
+);
+
+/* Join table. Student to Course relation */
+CREATE TABLE Prerequisite(
+	COURSE_ID INTEGER NOT NULL,
+	PREREQ_COURSE_ID INTEGER NOT NULL,
+	FOREIGN KEY(COURSE_ID) REFERENCES Course(ID),
+	FOREIGN KEY(PREREQ_COURSE_ID) REFERENCES Course(ID)
+);
