11384281f162d69328f86055cf81ef9d4ae21e23
axv3658 <axv3658@users.noreply.github.com>
2017-03-30 23:40:36 -0400

    Facility Management fork into master (#62)
    * example addClassroom
    
    * Added db scheme for FacMan
    
    * added api stubs
    
    * Fleshed out the api functions
    
    * schema update
    
    * updated schema
    
    * schema update
    
    * Fixed some column names in sql statements
    
    * change sql to merge in base repo
    
    * change sql so I can merge
    
    * Manual merged sql
    
    * Added the update and delete device api functions. Also moved the functions to have classroom and device functions together.
    
    * Added the Update and remove classroom functions. They still need to be tested.
    
    * Temporary fix to access db
    
    * Cleaner temporary fix for accessing db
    
    * fixed getting classrooms
    
    * Added functions for getting all classrooms and devices, fixed errors with getDevices
    
    * changes to searchclassrooms
    
    * Fixed functions for deleting classrooms and devices
    
    * Fixed functionality for updating classrooms and devices
    
    * Added functionality for searching and making reservations
    
    * Merge roda into us
    
    * added changes to api back
    
    * Fixed batch file for creating db

--- Database/createDB.bat
+++ Database/createDB.bat
@@ -16,5 +16,7 @@ IF "%selection%" == "y" (
 	sqlite3 %fileName% < "TeamEnrollment.sql"
 	sqlite3 %fileName% < "TeamFacilityManagement.sql"
 	sqlite3 %fileName% < "TeamGrading.sql"
+	
+	echo Finished creating tables.
 )
 pause 
