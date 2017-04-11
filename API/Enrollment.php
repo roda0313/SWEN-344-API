391092bd59f735e4b325d889ce4b9e66a37f428f
marissa <mxb9328@g.rit.edu>
2017-04-10 20:30:55 -0400

    adding section/instructor join table

--- API/Enrollment.php
+++ API/Enrollment.php
@@ -845,7 +845,7 @@ function getSectionInstructor($sectionID)
 		$sqlite->enableExceptions(true);
 		
 		//prepare query to protect from sql injection
-		$query = $sqlite->prepare("SELECT * FROM Section_Instructor WHERE SECTION_ID=:sectionID");
+		$query = $sqlite->prepare("SELECT * FROM Section_Professor WHERE SECTION_ID=:sectionID");
 		$query->bindParam(':sectionID', $sectionID);
 		$result = $query->execute();
 		
